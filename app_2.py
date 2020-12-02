52fbf00f5f98916d5ab2a17d398b0fc0ac33fbd4
SivaK18 <sivakailash18@gmail.com>
2020-12-02 14:16:56 +0530

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -60,7 +60,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook(credentials.URL)
+    updater.bot.setWebhook(credentials.URL+TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
